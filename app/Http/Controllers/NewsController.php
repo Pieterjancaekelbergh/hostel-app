63ff516669c5aa6a19d2431e828a4144be09df57
Frederick Roegiers <frederick@rogerthat.be>
2023-03-06 15:37:51 +0100

    wip

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,7 +9,43 @@ class NewsController extends Controller
 
     public function index()
     {
-        return view('news.index');
+        $newsItems = [
+          [
+            'id' => 1,
+            'title '=> 'New website',
+            'slug' => 'new-website',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+          ],
+          [
+            'id' => 2,
+            'title '=> 'Promotions on our luxury bedroom',
+            'slug' => 'promotions-on-bedroom',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+          ],
+          [
+            'id' => 3,
+            'title '=> 'Checkout our swimming pool, it is awesome and full of fishes', 
+            'slug' => 'fishy-pool',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+          ],
+        ];
+
+        $menuItems = [
+            [
+                'title' => 'Home',
+                'url' => '/',
+                'active' => false
+            ],
+            [
+                'title' => 'News',
+                'url' => '/news',
+                'active' => true
+            ]            
+        ];
+
+
+
+        return view('news.index', compact('newsItems', 'menuItems'));
     }
 
     public function show($slug)
