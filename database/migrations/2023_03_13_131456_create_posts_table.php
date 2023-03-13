022902bf111ea05b28e2aac436cc56d87108b4f3
Frederick Roegiers <frederick@rogerthat.be>
2023-03-13 14:22:53 +0100

    wip

---
+++ database/migrations/2023_03_13_131456_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title'); // varchar(255)
+            $table->string('slug'); // varchar(255)
+            $table->text('content'); // text
+            // relation many to one to categories
+            $table->foreignId('category_id')->constrained('categories');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
